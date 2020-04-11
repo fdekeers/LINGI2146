79bf556d5d819c3e89d48c89626771bedf1465c1
GrosPoulet42 <francois.dekeersmaeker@student.uclouvain.be>
2020-04-11 14:54:02 +0200

    Deleted contiki files

--- routing/sensor-node.c
+++ routing/sensor-node.c
@@ -18,7 +18,21 @@ AUTOSTART_PROCESSES(&sensor_node);
 
 // Broadcast Rime connexion
 static struct broadcast_conn broadcast;
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 
 PROCESS_THREAD(sensor_node, event, data) {
+
+	static struct etimer timer;
+	PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+	PROCESS_BEGIN();
+
+	broadcast_open(&broadcast, 129, &broadcast_call);
+
+	while(1) {
+
+		etimer_set(&timer, CLOCK_SECOND * 4 + random_rand());
+
+	}
 
 }
