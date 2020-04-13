5651ab76399dac7ac6c84dc6cebaf67d194e7ead
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2020-04-13 20:02:43 +0200

    Hashmap, still to test

---
+++ mote/hashmap.c
@@ -0,0 +1,218 @@
+#include "hashmap.h"
+
+/*
+ * Convert linkaddr to a uint16_t
+ */
+uint16_t linkaddr2uint16_t (linkaddr_t x) {
+    uint8_t a = x.u8[0]; uint8_t b = x.u8[1];
+    uint16_t newval = (((uint16_t) a) << 8 ) | ((uint16_t) b);
+    return newval;
+}
+
+/*
+ * Return an empty hashmap, or NULL on failure.
+ */
+hashmap_map * hashmap_new() {
+	hashmap_map *m = (hashmap_map*) malloc(sizeof(hashmap_map));
+	if(!m) goto err;
+
+	m->data = (hashmap_element*) calloc(INITIAL_SIZE, sizeof(hashmap_element));
+	if(!m->data) goto err;
+
+	m->table_size = INITIAL_SIZE;
+	m->size = 0;
+
+	return m;
+	err:
+		if (m)
+			hashmap_free(m);
+		return NULL;
+}
+
+/*
+ * Return the integer of the location in data
+ * to store the point to the item, or MAP_FULL.
+ */
+int hashmap_hash(hashmap_map *m, uint16_t key){
+	int curr;
+	int i;
+
+	/* If full, return immediately */
+	if(m->size >= (m->table_size/2)) return MAP_FULL;
+
+	/* Find the best index */
+	curr = key % m->table_size;
+
+	/* Linear probing */
+	for(i = 0; i< MAX_CHAIN_LENGTH; i++){
+		if(m->data[curr].in_use == 0)
+			return curr++;  //mutated statement
+
+		if(m->data[curr].in_use == 1 && (m->data[curr].key == key))
+			return curr;
+		
+		curr = (curr + 1) % m->table_size;
+	}
+
+	return MAP_FULL;
+}
+
+
+
+/*
+ * Doubles the size of the hashmap, and rehashes all the elements
+ */
+int hashmap_rehash(hashmap_map *m){
+	int i;
+	int old_size;
+	hashmap_element* curr;
+
+	/* Setup the new elements */
+	hashmap_element* temp = (hashmap_element *)
+		calloc(2 * m->table_size, sizeof(hashmap_element));
+	if(!temp) return MAP_OMEM;
+
+	/* Update the array */
+	curr = m->data;
+	m->data = temp;
+
+	/* Update the size */
+	old_size = m->table_size;
+	m->table_size = 2 * m->table_size;
+	m->size = 0;
+
+	/* Rehash the elements */
+	for(i = 0; i < old_size; i++)
+	{
+        int status;
+        if (curr[i].in_use == 0)
+            continue;
+            
+		status = hashmap_put_int(m, curr[i].key, curr[i].data);
+		if (status != MAP_OK)
+			return status;
+	}
+
+	free(curr);
+
+	return MAP_OK;
+}
+
+/*
+ * Add a pointer to the hashmap with some key
+ */
+int hashmap_put_int(hashmap_map *m, uint16_t key, linkaddr_t value){
+	int index;
+
+	/* Find a place to put our value */
+	index = key % m->table_size;
+	while(index == MAP_FULL){
+		if (hashmap_rehash(m) == MAP_OMEM) {
+			return MAP_OMEM;
+		}
+		index = hashmap_hash(m, key);
+	}
+
+	/* Set the data */
+	m->data[index].data = value;
+	m->data[index].key = key;
+	m->data[index].in_use = 1;
+	m->size++; 
+
+	return MAP_OK;
+}
+
+/*
+ * Add a pointer to the hashmap with some key.
+ */
+int hashmap_put(hashmap_map *m, linkaddr_t key, linkaddr_t value){
+    return hashmap_put_int(m, linkaddr2uint16_t(key), value);
+}
+/*
+ * Get your pointer out of the hashmap with a key
+ */
+int hashmap_get_int(hashmap_map *m, uint16_t key, linkaddr_t *arg){
+	int curr;
+	int i;
+
+	/* Find data location */
+	curr = key % m->table_size;
+
+	/* Linear probing, if necessary */
+	for(i = 0; i<MAX_CHAIN_LENGTH; i++){
+
+        int in_use = m->data[curr].in_use;
+        if (in_use == 1){
+            if (m->data[curr].key == key){
+                *arg = (m->data[curr].data);
+                return MAP_OK;
+            }
+		}
+
+		curr = (curr + 1) % m->table_size;
+	}
+
+	//*arg = NULL;
+
+	/* Not found */
+	return MAP_MISSING;
+}
+
+/*
+ * Get your pointer out of the hashmap with a key
+ */
+int hashmap_get(hashmap_map *m, linkaddr_t key, linkaddr_t *arg){
+    return hashmap_get_int(m, linkaddr2uint16_t(key), arg);
+}
+
+/*
+ * Remove an element with that key from the map
+ */
+int hashmap_remove_int(hashmap_map *m, uint16_t key){
+	int i;
+	int curr;
+
+	/* Find key */
+	curr = key % m->table_size;
+
+	/* Linear probing, if necessary */
+	for(i = 0; i<MAX_CHAIN_LENGTH; i++){
+
+        int in_use = m->data[curr].in_use;
+        if (in_use == 1){
+            if (m->data[curr].key == key){
+                /* Blank out the fields */
+                m->data[curr].in_use = 0;
+                //m->data[curr].data = NULL;
+                //m->data[curr].key = NULL;
+
+                /* Reduce the size */
+                m->size--;
+                return MAP_OK;
+            }
+		}
+		curr = (curr + 1) % m->table_size;
+	}
+
+	/* Data not found */
+	return MAP_MISSING;
+}
+
+/*
+ * Remove an element with that key from the map
+ */
+int hashmap_remove(hashmap_map *m, linkaddr_t key){
+    return hashmap_remove_int(m, linkaddr2uint16_t(key));
+}
+
+/* Deallocate the hashmap */
+void hashmap_free(hashmap_map *m){
+	free(m->data);
+	free(m);
+}
+
+/* Return the length of the hashmap */
+int hashmap_length(hashmap_map *m){
+	if(m != NULL) return m->size;
+	else return 0;
+}
---
+++ mote/hashmap.h
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "contiki.h"
+/*
+ * Adaptation by : Alexandre Gobeaux, François De Keersmaeker, Nicolas van de Walle
+ * 
+ * This is a hashmap from uint16_t to linkaddr_t. We adapted it from the string
+ * generic hashmap cited below.
+ * It is permitted to use linkaddr_t instead of uint16_t. However, we assume that those
+ * will consist of an array of 2 unsigned chars. (LINKADDR_SIZE = 2) 
+ *
+ * Originally by Elliot C Back - http://elliottback.com/wp/hashmap-implementation-in-c/
+ *
+ * Modified by Pete Warden to fix a serious performance problem, support strings as keys
+ * and removed thread synchronization - http://petewarden.typepad.com
+ */
+
+#define MAP_MISSING -3  /* No such element */
+#define MAP_FULL -2 	/* Hashmap is full */
+#define MAP_OMEM -1 	/* Out of Memory */
+#define MAP_OK 0 	/* OK */
+
+/* We need to keep keys and values */
+typedef struct _hashmap_element{
+	uint16_t key;
+	int in_use;
+	linkaddr_t data;
+} hashmap_element;
+
+/* A hashmap has some maximum size and current size,
+ * as well as the data to hold. */
+typedef struct _hashmap_map{
+	int table_size;
+	int size;
+	hashmap_element *data;
+} hashmap_map;
+
+extern hashmap_map * hashmap_new();
+extern int hashmap_put_int(hashmap_map *m, uint16_t key, linkaddr_t value);
+extern int hashmap_get_int(hashmap_map *m, uint16_t key, linkaddr_t *arg);
+extern int hashmap_remove_int(hashmap_map *m, uint16_t key);
+extern int hashmap_put(hashmap_map *m, linkaddr_t key, linkaddr_t value);
+extern int hashmap_get(hashmap_map *m, linkaddr_t key, linkaddr_t *arg);
+extern int hashmap_int(hashmap_map *m, linkaddr_t key);
+extern uint16_t linkaddr2uint16_t (linkaddr_t x);
+extern void hashmap_free(hashmap_map *m);
+extern int hashmap_length(hashmap_map *m);
+
+#define INITIAL_SIZE (256) // initial size of hashmap
+#define MAX_CHAIN_LENGTH (8) // number of "looks after" for linear probing
